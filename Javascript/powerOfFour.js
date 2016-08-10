4e712d7832d2c347f10040f15f8a7266ccfbe27c
unluckymattress <unluckymattress@gmail.com>
2016-08-10 08:29:08 -0500

    Challenge 3 - PowerOfFour complete

---
+++ Javascript/powerOfFour.js
@@ -0,0 +1,23 @@
+/*
+	Challenge: 3
+	Source: leetcode
+	Difficulty: Easy
+
+	Instructions:
+	Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+	Example:
+	Given num = 16, return true. Given num = 5, return false.
+*/
+
+var powerOfFour = function(num) {
+	if ( num % 4 === 0 ) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+console.log(powerOfFour(20));
+console.log(powerOfFour(41));
